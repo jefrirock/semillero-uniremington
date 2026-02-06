d5cd50323eae2b8893020e53bad427a276b9228d
Jefri Rock <jefrirock@gmail.com>
2026-02-05 20:52:51 -0500

    02/01/2026-2:24 PM

--- src/main/java/com/uniremington/semillero/model/Docente.java
+++ src/main/java/com/uniremington/semillero/model/Docente.java
@@ -14,12 +14,15 @@ public class Docente {
     private String nombre;
 
     private String area;
+    private String especialidad;      // <-- AGREGADO
     private String email;
-    private String descripcion;
+    private String telefono;          // <-- AGREGADO
+    private Integer experienciaAnios; // <-- AGREGADO
     private String imagenUrl;
 
     public Docente() {}
 
+    // Getters y Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
@@ -29,11 +32,17 @@ public class Docente {
     public String getArea() { return area; }
     public void setArea(String area) { this.area = area; }
 
+    public String getEspecialidad() { return especialidad; }      // <-- AGREGADO
+    public void setEspecialidad(String especialidad) { this.especialidad = especialidad; }
+
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
 
-    public String getDescripcion() { return descripcion; }
-    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+    public String getTelefono() { return telefono; }              // <-- AGREGADO
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+
+    public Integer getExperienciaAnios() { return experienciaAnios; } // <-- AGREGADO
+    public void setExperienciaAnios(Integer experienciaAnios) { this.experienciaAnios = experienciaAnios; }
 
     public String getImagenUrl() { return imagenUrl; }
     public void setImagenUrl(String imagenUrl) { this.imagenUrl = imagenUrl; }
